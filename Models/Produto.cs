f347b4d72e1131d225b22e2051f9d2e3dfd5131d
recruta-wen <xxkauanxxgamer@gmail.com>
2024-06-13 10:41:27 -0300

    Alteração Model Produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,6 +1,32 @@
-﻿namespace CardapioWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWeb.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
+        public int Id { get; set; }
+
+            [Required(ErrorMessage = "Campo Obrigatório!")]
+            [Display(Name = "Nome do Produto")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        [Display(Name = "Descrição Curta")]
+        public string Descricao_curta { get; set; }
+
+        public string Descricao_detalhada { get; set; }
+        public decimal Preco { get; set; }
+        public bool Produto_preferido { get; set; }
+
+            [Required(ErrorMessage = "Campo Obrigatório!")]
+            [Display(Name = "Em estoque")]
+        public bool Em_estoque { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
+
+
     }
 }
